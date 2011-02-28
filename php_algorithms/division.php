3936c9a1c9f9b3aa5788eb0f23f6dcd9d95bef3a
Felipe Ribeiro <felipernb@gmail.com>
2011-02-28 16:39:56 +0100

    Adding folder for algorithms implementations in PHP, starting with Division without the division operator

---
+++ php_algorithms/division.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * Implementing integer division without division operator
+ * @author Felipe Ribeiro <felipernb@gmail.com>
+ */
+function division($dividend, $divisor) {
+        
+    if ($divisor == $dividend) {
+       return 1;
+    } else if ($dividend < $divisor) {
+       return 0;
+    }    
+    
+	$quotient = 1;
+    while ($divisor*$quotient <= $dividend) {
+       $quotient = $quotient << 1;
+    }
+	
+	/* quotient * divisor > $dividend */
+    $quotient = $quotient >> 1;
+		
+    /* Call division recursively for the difference to get the
+    exact quotient */
+    $quotient = $quotient + division($dividend - $divisor*$quotient, $divisor);
+    
+    return $quotient;
+}  
+
+
+$in = fopen('php://stdin', 'r');
+while (($dividend = (int)fgets($in))) {
+	$divisor = (int)fgets($in);
+	$division = division($dividend,$divisor);
+	//validates correctness
+	assert($division == floor($dividend/$divisor));
+	echo $division	. "\n";
+}
