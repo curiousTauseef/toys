9c24619fa98fcd8ae95fe34760ae1819710c53cf
Felipe Ribeiro <felipernb@gmail.com>
2011-02-28 16:42:38 +0100

    Fixing indentation

--- php_algorithms/division.php
+++ php_algorithms/division.php
@@ -5,27 +5,28 @@
  * @author Felipe Ribeiro <felipernb@gmail.com>
  */
 function division($dividend, $divisor) {
-        
-    if ($divisor == $dividend) {
-       return 1;
-    } else if ($dividend < $divisor) {
-       return 0;
-    }    
-    
+
+	if ($divisor == $dividend) {
+		return 1;
+	} else if ($dividend < $divisor) {
+		return 0;
+	}
+
 	$quotient = 1;
-    while ($divisor*$quotient <= $dividend) {
-       $quotient = $quotient << 1;
-    }
-	
+
+	while ($divisor*$quotient <= $dividend) {
+		$quotient = $quotient << 1;
+	}
+
 	/* quotient * divisor > $dividend */
-    $quotient = $quotient >> 1;
-		
-    /* Call division recursively for the difference to get the
-    exact quotient */
-    $quotient = $quotient + division($dividend - $divisor*$quotient, $divisor);
-    
-    return $quotient;
-}  
+	$quotient = $quotient >> 1;
+
+	/* Call division recursively for the difference to get the
+	 exact quotient */
+	$quotient = $quotient + division($dividend - $divisor*$quotient, $divisor);
+
+	return $quotient;
+}
 
 
 $in = fopen('php://stdin', 'r');
